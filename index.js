1c9d3a7091d0b1a4880c65bede752749188f6c34
Jono <jonothanallen@gmail.com>
2021-10-19 17:01:06 +1300

    change to batman

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 async function run() {
   try {
-    const percentage = core.getInput('percentage');
+    const percentage = core.getInput("percentage");
     var message;
-    if(percentage > Math.floor(Math.random() * 100)){
-        message = '![rickroll](https://user-images.githubusercontent.com/37572049/90699500-0cc3ec00-e2a1-11ea-8d13-989526e86b0e.gif)';
-        console.log('Gottem!!');
-        
-    }else{
-        console.log('Fine you win this time.');
-        message = core.getInput('message');
-        if(message == ''){
-            return;
-        }
+    if (percentage > Math.floor(Math.random() * 100)) {
+      message =
+        "![batman](https://media.giphy.com/media/jIzXYqaQ0nLkA/giphy.gif)";
+      console.log("Gottem!!");
+    } else {
+      console.log("Fine you win this time.");
+      message = core.getInput("message");
+      if (message == "") {
+        return;
+      }
     }
 
-    const github_token = core.getInput('GITHUB_TOKEN');
+    const github_token = core.getInput("GITHUB_TOKEN");
 
     const context = github.context;
-    if (context.payload.issue == null) {
-        core.setFailed('No issue found.');
-        return;
+    if (context.payload.pull_request == null) {
+      core.setFailed("No pull_request found.");
+      return;
     }
-    const issue_number = context.payload.issue.number;
+    console.log(context);
+    const pull_request_number = context.payload.pull_request.number;
 
     const octokit = new github.getOctokit(github_token);
     const new_comment = octokit.issues.createComment({
-        ...context.repo,
-        issue_number: issue_number,
-        body: message
-      });
-
+      ...context.repo,
+      issue_number: pull_request_number,
+      body: message,
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
